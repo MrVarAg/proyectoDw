7e8f7957080dfdb97bb0596298fb0aea0a1ba974
donal <donaldovarela2002@gmail.com>
2024-11-06 21:12:48 -0600

    Llamada de login al formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 
 import { useState, useEffect } from 'react';
-// Importar los módulos de Firebase
+
 import appFirebase from '../src/log-credenciales';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 const auth = getAuth(appFirebase);
 
-// Importar los componentes Login y QrReader
 import Login from '../src/componetnts/Login';
-import QrReader from './componetnts/qrReader/qrReader';
+import QrReader from './componetnts/HorarioForm';
 import './App.css';
 
 function App() {
   const [usuario, setUsuario] = useState(null);
 
-  // Usar useEffect para gestionar el estado de autenticación
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (usuarioFirebase) => {
       if (usuarioFirebase) {
@@ -23,9 +21,8 @@ function App() {
       }
     });
 
-    // Limpiar la suscripción cuando el componente se desmonte
     return () => unsubscribe();
-  }, []); // Solo se ejecuta una vez cuando el componente se monta
+  }, []); 
 
   return (
     <div>
