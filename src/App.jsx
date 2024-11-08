0805a66c1dfd9d95d18061f1a5e7f5a38f6b6071
donal <donaldovarela2002@gmail.com>
2024-11-08 12:49:13 -0600

    Prueba

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,65 @@
-
 import { useState, useEffect } from 'react';
-
 import appFirebase from '../src/log-credenciales';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-const auth = getAuth(appFirebase);
 
-import Login from '../src/componetnts/Login';
-import QrReader from './componetnts/HorarioForm';
+import Login from './componetnts/Login';
+import QrReader from './componetnts/qrReader/qrReader';
+import HorarioForm from './componetnts/HorarioForm';
+import Reports from './componetnts/Reports'; // Importa el componente Reports
+import Menu from './componetnts/menu';
 import './App.css';
 
+const auth = getAuth(appFirebase);
+
 function App() {
-  const [usuario, setUsuario] = useState(null);
+    const [usuario, setUsuario] = useState(null);
+    const [selectedOption, setSelectedOption] = useState('menu'); // Estado para el menú seleccionado
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (usuarioFirebase) => {
-      if (usuarioFirebase) {
-        setUsuario(usuarioFirebase);
-      } else {
-        setUsuario(null);
-      }
-    });
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (usuarioFirebase) => {
+            setUsuario(usuarioFirebase ? usuarioFirebase : null);
+        });
 
-    return () => unsubscribe();
-  }, []); 
+        return () => unsubscribe();
+    }, []);
 
-  return (
-    <div>
-      {usuario ? <QrReader correoUsuario={usuario.email} /> : <Login />}
-    </div>
-  );
+    const handleSelectOption = (option) => {
+        setSelectedOption(option);
+    };
+
+    const handleReturnToMenu = () => {
+        setSelectedOption('menu');
+    };
+
+    return (
+        <div>
+            {usuario ? (
+                <>
+                    {selectedOption === 'menu' && <Menu onSelectOption={handleSelectOption} />}
+                    {selectedOption === 'assignSchedule' && (
+                        <>
+                            <HorarioForm />
+                            <button onClick={handleReturnToMenu}>Volver al Menú</button>
+                        </>
+                    )}
+                    {selectedOption === 'scanQr' && (
+                        <>
+                            <QrReader />
+                            <button onClick={handleReturnToMenu}>Volver al Menú</button>
+                        </>
+                    )}
+                    {selectedOption === 'reports' && (
+                        <>
+                            <Reports />
+                            <button onClick={handleReturnToMenu}>Volver al Menú</button>
+                        </>
+                    )}
+                </>
+            ) : (
+                <Login />
+            )}
+        </div>
+    );
 }
 
 export default App;
